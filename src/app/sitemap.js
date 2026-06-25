e62b3a4765cb9a4abe805727a2d60349fb48defc
ZetorShabba <csaba.martonosi@gmail.com>
2026-06-26 00:21:30 +0200

    Add sitemap and robots metadata files

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.marlenatelier.hu/",
+      lastModified: new Date("2026-06-26"),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
