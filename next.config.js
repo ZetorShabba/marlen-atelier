fc83015471918aa2c092cc5d682b87faaa91a2a7
ZetorShabba <csaba.martonosi@gmail.com>
2026-06-25 18:29:21 +0200

    Fix image paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const repoName = "marlen-atelier";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
