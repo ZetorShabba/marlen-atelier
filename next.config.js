095b3bd346c3811e9a50e80495ac0ac042735b8e
ZetorShabba <csaba.martonosi@gmail.com>
2026-06-25 20:35:36 +0200

    update paths for domain

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const repoName = "marlen-atelier";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
   trailingSlash: true,
 };
 
